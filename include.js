58727022537050837d180b85c9a5280a15b0524b
lithtok <LS09846@learning.lithan.com>
2025-07-11 05:40:36 +0800

    Add files via upload

---
+++ include.js
@@ -0,0 +1,14 @@
+
+function includeHTML(id, file) {
+  const element = document.getElementById(id);
+  fetch(file)
+    .then(response => response.text())
+    .then(data => {
+      element.innerHTML = data;
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  includeHTML("header", "../pages/header.html");
+  includeHTML("footer", "../pages/footer.html");
+});
